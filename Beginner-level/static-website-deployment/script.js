c5dd078ce9993311026d3d4e6c30b1f5ad241fe8
ema <ibera.abrams@yahoo.com>
2023-09-19 09:35:26 +0100

    updated website content

--- Beginner-level/static-website-deployment/script.js
+++ Beginner-level/static-website-deployment/script.js
@@ -1,6 +1,22 @@
 // script.js
-function showAlert() {
-    alert("Hello, welcome to my demo Static website!");
+
+// Function to smoothly scroll to a target element
+function scrollToTarget(target) {
+    window.scroll({
+        top: target.offsetTop,
+        behavior: 'smooth' // Smooth scroll behavior
+    });
 }
 
-// Add more JavaScript code as needed
+// Attach click event listeners to navigation links
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach((link) => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault(); // Prevent default link behavior
+        const targetId = this.getAttribute('href').substring(1); // Remove the '#' character
+        const target = document.getElementById(targetId);
+        if (target) {
+            scrollToTarget(target);
+        }
+    });
+});
